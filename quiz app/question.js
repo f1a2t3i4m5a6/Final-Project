c47ad74e7b8d7ce552abf41dd4aa05db835d2223
f1a2t3i4m5a6 <fatimahanif1905@gmail.com>
2025-10-18 20:16:38 -0700

    Add files via upload

---
+++ quiz app/question.js
@@ -0,0 +1,57 @@
+let question=[
+    {
+        numb:1,
+        questions:"Q:Who was the founder of Pakistan?",
+        answer:"3. Quaid e Azam Muhammad Ali Jinnah",
+        Options:[
+"1. Sir Syed Ahmed Khan",
+"2. Liaquat Ali Khan",
+"3. Quaid e Azam Muhammad Ali Jinnah",
+"4. Nawab Waqar ul Mulk"
+        ]
+    },
+     {
+        numb:2,
+        questions:"Q:What is the color of our national flag? ",
+        answer:"4. Green and White",
+        Options:[
+"1. Pink and Grey",
+"2. Green and Grey",
+"3. Red and Blue",
+"4. Green and White"
+        ]
+    },
+     {
+        numb:3,
+        questions:"Q:Who was the first Prime minister of Pakistan?",
+        answer:"2. Liaquat Ali Khan",
+        Options:[
+"1. Quaid e Azam",
+"2. Liaquat Ali Khan",
+"3. Molana Muhammad Ali Johar",
+"4. Fatima Jinnah"
+        ]
+    },
+     {
+        numb:4,
+        questions:"Q:What is the national dress of Pakistan?",
+        answer:"1. Shalwar,Qameez",
+        Options:[
+"1. Shalwar,Qameez",
+"2. Paint,Shirt",
+"3. Saree",
+"4. Kurti,Shalwar"
+        ]
+    },
+     {
+        numb:5,
+        questions:"Q:Who wrote Pakistan's national anthem?",
+        answer:"3. Hafeez Jalandhari ",
+        Options:[
+"1. Mir taqi mir",
+"2. Sir Syed Ahmed Khan",
+"3. Hafeez Jalandhari ",
+"4. Nawab Saleem Ullah"
+        ]
+    },
+];
